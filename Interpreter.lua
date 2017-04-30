59300fa895acc550710e958d690b234e3d89ff9d
Rerumu <keys3a@gmail.com>
2017-04-30 12:41:37 -0400

    Source
    Woo. Finally done.

---
+++ Interpreter.lua
@@ -0,0 +1,199 @@
+local StrByte	= string.byte;
+local Remove	= table.remove;
+local Insert	= table.insert;
+local Concat	= table.concat;
+local Gmatch	= string.gmatch;
+local Match		= string.match;
+local Gsub		= string.gsub;
+local Tonu		= tonumber;
+local Str		= tostring;
+local New		= Instance.new;
+local Sub		= string.sub;
+local Upk		= unpack;
+
+local function Process(Env, ...)
+	local Args	= {...};
+	local Ret	= {};
+
+	for Idx = 1, #Args do
+		local Arg	= Args[Idx];
+
+		if (Arg ~= 'nil') then
+			if (Arg == 'true') then
+				Ret[Idx]	= true;
+			elseif (Arg == 'false') then
+				Ret[Idx]	= false;
+			elseif Tonu(Arg) then
+				Ret[Idx]	= Tonu(Arg);
+			elseif (Env[Arg] ~= nil) then
+				Ret[Idx]	= Env[Arg];
+			else
+				local A, B	= Match(Arg, '^(.)(.*).$');
+
+				if (A == '"') then
+					Ret[Idx]	= B;
+				elseif (A == '[') then
+					A, B	= Match(B, '^(.)(.*)$');
+
+					if (A == '*') then
+						local Curr	= Env;
+
+						for Path in Gmatch(B, '[^/]+') do
+							Curr	= Curr[Path];
+						end;
+
+						Ret[Idx]	= Curr;
+					else
+						local Name, Args	= Match(A .. B, '^(%S+)%s*(.*)$');
+
+						if Name then
+							local Funct			= Env[Name];
+							local NumA			= #Args;
+							local Prc			= {};
+							local Bf			= {};
+							local Qo, Qa, Es;
+
+							for Idz = 1, NumA do
+								local Ke	= Sub(Args, Idz, Idz);
+
+								Bf[#Bf + 1]	= Ke;
+
+								if (Ke == "'") and (not Qo) and (not Es) then
+									Qa	= (not Qa);
+								elseif (Ke == '"') and (not Qa) and (not Es) then
+									Qo	= (not Qo);
+								end;
+
+								if ((Ke == ',') and (not Qo) and (not Qa)) or (Idz == NumA) then
+									if (Ke == ',') then
+										Bf[#Bf]	= nil;
+									end;
+
+									Prc[#Prc + 1]	= Match(Concat(Bf), '^%s*(.-)%s*$');
+
+									Bf				= {};
+								end;
+
+								if (not Es) then
+									Es	= (Ke == '\\');
+								else
+									Es	= false;
+								end;
+							end;
+
+							Ret[Idx]	= Funct(Process(Env, Upk(Prc)));
+						end;
+					end;
+				end;
+			end;
+		end;
+	end;
+
+	return Upk(Ret);
+end;
+
+return function(Byte, Env)
+	assert('Invalid bytecode string.', Sub(Byte, 1, 3) == '\217md');
+
+	local Constants	= {};
+	local Buff		= {};
+	local Quo, Esc;
+	local Final;
+
+	for Idx = 1, #Byte do
+		local Key	= Sub(Byte, Idx, Idx);
+
+		Buff[#Buff + 1]	= Key;
+
+		if (not Esc) then
+			if (Key == '"') then
+				if Quo then
+					Constants[#Constants + 1]	= Sub(Concat(Buff), 1, -2);
+
+					Quo	= false;
+				else
+					Buff	= {};
+
+					Quo		= true;
+				end;
+			elseif (Key == '\255') then
+				Buff	= nil;
+
+				Final	= Sub(Byte, Idx + 1);
+
+				break;
+			end;
+
+			Esc	= (Key == '\\');
+		else
+			Esc	= false;
+		end;
+	end;
+
+	Buff	= {};
+
+	for Str in Gmatch(Final, '%Z+') do
+		local Args	= Sub(Str, 2);
+		local Proc	= 0;
+		local Now	= {StrByte(Str)};
+
+		for Idx = 1, #Args do
+			local Ke	= Sub(Args, Idx, Idx);
+
+			if (Ke == '\255') then
+				local Const	= Gsub(Constants[Proc], '\\([\\"])', '%1');
+
+				Now[#Now + 1]	= Const;
+
+				Proc			= 0;
+			else
+				Proc			= Proc + StrByte(Ke);
+			end;
+		end;
+
+		Buff[#Buff + 1]	= Now;
+	end;
+
+	Final	= Buff;
+	Buff	= nil;
+
+	return function()
+		local Stacks	= {};
+		local Current;
+
+		for Idx = 1, #Final do
+			local Data	= Final[Idx];
+			local Intr	= Data[1];
+			local A, B	= Data[2], Data[3];
+
+			if (Intr == 1) then -- NEW
+				local Cr	= New(A);
+
+				Cr.Parent	= Current;
+
+				Current	= Cr;
+				Insert(Stacks, 1, Cr);
+			elseif (Intr == 2) then -- ANEW
+				local Cr	= New(A);
+
+				Cr.Parent	= Current;
+
+				Current	= Cr;
+			elseif (Intr == 3) then -- SET
+				Current[A]	= Process(Env, B);
+			elseif (Intr == 4) then -- NEG
+				Current[A]	= (not Current[A]);
+			elseif (Intr == 5) then -- GLO
+				Env[A]		= Process(Env, B);
+			elseif (Intr == 6) then -- CLS
+				if (Str(Stacks[1]) == B) and (#Stacks > 1) then
+					Stacks[1]	= nil;
+				end;
+
+				Current	= Stacks[1];
+			end;
+		end;
+
+		return Stacks[#Stacks];
+	end;
+end;
---
+++ README.md
@@ -0,0 +1,69 @@
+# Intro
+
+Woah. **Technology.**
+This is the 3rd language I've made so far.
+The language has a style similar to that of HTML and the like.
+While it is made for use in ROBLOX to create instances you can use it with whatever system you have by simply changing the `Instance.new` local inside of it.
+
+The language was inspired by [cntkillme](https://www.roblox.com/users/294568/profile) but with a different take on the structure.
+This source is entirely made by [Rerumu](https://www.roblox.com/users/294568/profile).
+It is to be noted that the compiler and interpreter are independent and do not rely on eachother. That said, you can pre-compile your code into bytecode strings and they will run normally at runtime or be sent over machines to be interpreted.
+
+As always; I am not perfect so please inform me of any bugs!
+
+## Example
+Html is the closest syntax highlighting I can get to.
+
+```Html
+#UDim2:[*UDim2/new] ; The * means it is a path.
+#Serv:[*game/GetService] ; Because I am lazy.
+#Storage:[Serv game, "StarterGui"] ; Statements can only use " and no ' for strings.
+
+; Semicolons are used to start and end comments unless a newline does so.
+
+<ScreenGui Name:'Test' Parent:Storage> ; Globals are handled as usual.
+	<~Frame ; The ~ means it is automatically closed.
+		Name:'Top'
+		Size:[UDim2 1, 0, 0.1, 0] ; Without the * it calls the function with the args given.
+	>
+	<~TextBox
+		Name:'Text' ; Strings are handled as usual.
+		Text:'Five hours later.'
+		Position:[UDim2 0, 0, 0.1, 0]
+		Size:[UDim2 1, 0, 0.1, 0]
+		!Visible ; This makes it "not Visible". Opposite of what it was.
+	>
+<ScreenGui/> ; This closes and goes back to the previous layer if any.
+```
+
+The Lua should look a bit like this.
+
+```Lua
+local Confi	= require(PathToCompiler); -- The paths whatever they may be.
+local Intr	= require(PathToInterpreter);
+
+local Code	= [[
+	#UDim2:[*UDim2/new]
+	#Serv:[*game/GetService]
+	#Storage:[Serv game, "StarterGui"]
+
+	<ScreenGui Name:'Test' Parent:Storage>
+		<~Frame ; The ~ means it is automatically closed.
+			Name:'Top'
+			Size:[UDim2 1, 0, 0.1, 0]
+		>
+		<~TextBox
+			Name:'Text'
+			Text:'Five hours later.'
+			Position:[UDim2 0, 0, 0.1, 0]
+			Size:[UDim2 1, 0, 0.1, 0]
+			!Visible
+		>
+	<ScreenGui/>
+]];
+
+local Byte	= Confi(Code);
+local Gui	  = Intr(Byte, getfenv());
+
+Gui(); -- Creates the Gui (and returns it).
+```
