bd7a0738aa478de654613b9c645f2b150b13c229
Rerumu <keys3a@gmail.com>
2017-11-11 22:40:37 -0500

    Rewrite
    Interpreter rewrite.

--- Interpreter.lua
+++ Interpreter.lua
@@ -1,198 +1,118 @@
-local StrByte	= string.byte;
-local Remove	= table.remove;
-local Insert	= table.insert;
-local Concat	= table.concat;
-local Gmatch	= string.gmatch;
-local Match		= string.match;
-local Gsub		= string.gsub;
-local Tonu		= tonumber;
-local Str		= tostring;
-local New		= Instance.new;
-local Sub		= string.sub;
-local Upk		= unpack;
+local Match	= string.match;
+local Tonum	= tonumber;
+local Byte	= string.byte;
+local Sub	= string.sub;
+local Upk	= unpack;
+local New	= Instance.new; -- Editable to whatever I guess
 
-local function Process(Env, ...)
-	local Args	= {...};
-	local Ret	= {};
+local function Extract(Bit, Start, End)
+	local Res	= (Bit / 2 ^ Start) % 2 ^ (End - Start + 1);
 
-	for Idx = 1, #Args do
-		local Arg	= Args[Idx];
-
-		if (Arg ~= 'nil') then
-			if (Arg == 'true') then
-				Ret[Idx]	= true;
-			elseif (Arg == 'false') then
-				Ret[Idx]	= false;
-			elseif Tonu(Arg) then
-				Ret[Idx]	= Tonu(Arg);
-			elseif (Env[Arg] ~= nil) then
-				Ret[Idx]	= Env[Arg];
-			else
-				local A, B	= Match(Arg, '^(.)(.*).$');
-
-				if (A == '"') then
-					Ret[Idx]	= B;
-				elseif (A == '[') then
-					A, B	= Match(B, '^(.)(.*)$');
-
-					if (A == '*') then
-						local Curr	= Env;
-
-						for Path in Gmatch(B, '[^/]+') do
-							Curr	= Curr[Path];
-						end;
-
-						Ret[Idx]	= Curr;
-					else
-						local Name, Args	= Match(A .. B, '^(%S+)%s*(.*)$');
-
-						if Name then
-							local Funct			= Env[Name];
-							local NumA			= #Args;
-							local Prc			= {};
-							local Bf			= {};
-							local Qo, Qa, Es;
-
-							for Idz = 1, NumA do
-								local Ke	= Sub(Args, Idz, Idz);
-
-								Bf[#Bf + 1]	= Ke;
-
-								if (Ke == "'") and (not Qo) and (not Es) then
-									Qa	= (not Qa);
-								elseif (Ke == '"') and (not Qa) and (not Es) then
-									Qo	= (not Qo);
-								end;
-
-								if ((Ke == ',') and (not Qo) and (not Qa)) or (Idz == NumA) then
-									if (Ke == ',') then
-										Bf[#Bf]	= nil;
-									end;
-
-									Prc[#Prc + 1]	= Match(Concat(Bf), '^%s*(.-)%s*$');
-
-									Bf				= {};
-								end;
-
-								if (not Es) then
-									Es	= (Ke == '\\');
-								else
-									Es	= false;
-								end;
-							end;
-
-							Ret[Idx]	= Funct(Process(Env, Upk(Prc)));
-						end;
-					end;
-				end;
-			end;
-		end;
-	end;
-
-	return Upk(Ret);
+	return Res - Res % 1;
 end;
 
-return function(Byte, Env)
-	assert('Invalid bytecode string.', Sub(Byte, 1, 3) == '\217md');
-
+return function(Bytes)
 	local Constants	= {};
-	local Buff		= {};
-	local Quo, Esc;
-	local Final;
-
-	for Idx = 1, #Byte do
-		local Key	= Sub(Byte, Idx, Idx);
-
-		Buff[#Buff + 1]	= Key;
-
-		if (not Esc) then
-			if (Key == '"') then
-				if Quo then
-					Constants[#Constants + 1]	= Sub(Concat(Buff), 1, -2);
-
-					Quo	= false;
-				else
-					Buff	= {};
-
-					Quo		= true;
-				end;
-			elseif (Key == '\255') then
-				Buff	= nil;
-
-				Final	= Sub(Byte, Idx + 1);
-
-				break;
-			end;
-
-			Esc	= (Key == '\\');
-		else
-			Esc	= false;
-		end;
+	local Instructs	= {};
+	local Sig, Ksts	= Match(Bytes, '^(RML)%z(%x+)');
+	local Posi		= 1;
+	
+	if (not Sig) then
+		error('Bytecode format not supported');
+	else
+		Bytes	= Sub(Bytes, 5 + #Ksts);
+		Ksts	= Tonum(Ksts, 16);
 	end;
-
-	Buff	= {};
-
-	for Str in Gmatch(Final, '%Z+') do
-		local Args	= Sub(Str, 2);
-		local Proc	= 0;
-		local Now	= {StrByte(Str)};
-
-		for Idx = 1, #Args do
-			local Ke	= Sub(Args, Idx, Idx);
-
-			if (Ke == '\255') then
-				local Const	= Gsub(Constants[Proc], '\\([\\"])', '%1');
-
-				Now[#Now + 1]	= Const;
-
-				Proc			= 0;
-			else
-				Proc			= Proc + StrByte(Ke);
-			end;
-		end;
-
-		Buff[#Buff + 1]	= Now;
-	end;
-
-	Final	= Buff;
-	Buff	= nil;
-
-	return function()
-		local Stacks	= {};
-		local Current;
-		local First; -- The return.
-
-		for Idx = 1, #Final do
-			local Data	= Final[Idx];
-			local Intr	= Data[1];
-			local A, B	= Data[2], Data[3];
+	
+	for Idx	= 1, Ksts do
+		local Type	= Byte(Bytes, Posi, Posi);
+		
+		if (Type == 0) then
+			Constants[Idx]	= 0;
+		elseif (Type == 1) then
+			Constants[Idx]	= Byte(Bytes, Posi + 1, Posi + 1) == 1;
 			
-			if (Intr == 1) then -- NEW
-				local Cr	= New(A);
-
-				Cr.Parent	= Current;
-
-				Current	= Cr;
-				Insert(Stacks, 1, Cr);
+			Posi	= Posi + 1;
+		elseif (Type == 2) then
+			Constants[Idx]	= Tonum(Sub(Bytes, Posi + 1, Posi + 8), 16);
+			
+			Posi	= Posi + 8;
+		elseif (Type == 3) then
+			local Len	= Byte(Bytes, Posi + 1, Posi + 1);
+			
+			Constants[Idx]	= Tonum(Sub(Bytes, Posi + 2, Posi + 1 + Len));
+			
+			Posi	= Posi + Len + 1;
+		elseif (Type == 4) then
+			local Len	= Tonum(Sub(Bytes, Posi + 1, Posi + 8), 16);
+			
+			Constants[Idx]	= Sub(Bytes, Posi + 9, Posi + Len + 8);
+			
+			Posi	= Posi + Len + 8;
+		end;
+		
+		Posi	= Posi + 1;
+	end;
+	
+	Ksts	= Tonum(Sub(Bytes, Posi, Posi + 7), 16);
+	Posi	= Posi + 8;
+	
+	for Idx = 1, Ksts do
+		local A, B, C	= Byte(Bytes, Posi, Posi + 2);
+		local Opco		= Extract(A, 0, 2);
+		local Stack		= Extract(A, 3, 7);
+		
+		Instructs[#Instructs + 1]	= {
+			Opco, Stack,
+			B + (C * 256);
+		};
+		
+		Posi	= Posi + 3;
+	end;
+	
+	return function(Env)
+		local Lays	= 0;
+		local Stack	= {};
+		local Tree	= {};
+		local Return;
+		
+		for Idx = 1, Ksts do
+			local Inst	= Instructs[Idx];
+			local Op	= Inst[1];
+			local Stk	= Inst[2];
+			local Lay	= Tree[Lays];
+			
+			if (Op == 0) then -- LOADK
+				Stack[Stk]	= Constants[Inst[3] + 1];
+			elseif (Op == 1) then -- NEW
+				local Inst	= New(Stack[Stk]);
 				
-				if (not First) then
-					First	= Cr;
+				if (Lays == 0) then
+					Return	= Inst;
 				end;
-			elseif (Intr == 2) then -- SET
-				Current[A]	= Process(B);
-			elseif (Intr == 3) then -- NEG
-				Current[A]	= (not Current[A]);
-			elseif (Intr == 4) then -- GLO
-				Env[A]		= Process(B);
-			elseif (Intr == 5) then -- CLS
-				if Stacks[1] and (Stacks[1].ClassName == A) then
-					Remove(Stacks, 1); -- Fixed some parenting stuff.
+				
+				Lays		= Lays + 1;
+				Tree[Lays]	= Inst;
+			elseif (Op == 2) then -- SET
+				Lay[Stack[Stk]]	= Stack[Inst[3]]; -- TODO: Fix stack not resetting on calls?
+			elseif (Op == 3) then -- INV
+				local Prop	= Stack[Stk];
+				
+				Lay[Prop]	= (not Lay[Prop]);
+			elseif (Op == 4) then -- CLS
+				if (Lay.ClassName == Stack[Stk]) then -- Check classname
+					Tree[Lays]		= nil;
+					Lays			= Lays - 1;
+					
+					Lay.Parent	= Tree[Lays];
+				else
+					error('Runtime error while closing');
 				end;
-
-				Current	= Stacks[1];
+			elseif (Op == 5) then -- CALL
+				Stack[Stk]	= Env[Stack[Inst[3]]](Upk(Stack, 0, Stk - 1));
 			end;
 		end;
-
-		return First;
+		
+		return Return;
 	end;
 end;
